d42cb6465292392de31e6b789082cbdcb7048679
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-07-28 12:47:39 +0300

    Created basic bootstrap card.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.main')
 
 @section('content')
-    <h2>Out content</h2>
+    <div class="container-lg" style="margin: 0 auto;">
+        <div class="row mt-5">
+
+            <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="" alt=""/>
+                    <div class="card-body">
+                        <div class="card-title">title</div>
+                        <div class="card-text">content</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
