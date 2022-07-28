40a465bf5dd5e4e7ef53195b3e8598b8f619de95
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-07-28 15:32:12 +0300

    Installed livewire

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@ use App\Http\Controllers\ProjectController;
 */
 
 Route::get('/', [ProjectController::class, 'getData']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
