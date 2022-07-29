dcf0a7f7c2c4761b984630ddafc849f8d5e40c8b
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-07-29 15:02:10 +0300

    appointment table

--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
     <div class="container-lg" style="margin: 0 auto;">
-
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                   <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
